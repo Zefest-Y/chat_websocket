65997a1f9d7ebb11287da43d72fca6f19dca8e9c
Zefest-Y <andreusgschultz@gmail.com>
2025-08-01 16:56:51 -0300

    modificação na estrutura front-end
    alteração na estrutura de mensagens e modulação no back-end para a criação de mensagens

--- client/client.js
+++ client/client.js
@@ -1,5 +1,5 @@
 const ws = new WebSocket("ws://localhost:4000")
-const mensagens = document.getElementById("messages")
+const mensagens = document.getElementById("message_container")
 const campo = document.getElementById("msgInput")
 
 console.log("iniciou...")
@@ -8,6 +8,22 @@ function envia(){
     let mensagem = JSON.stringify({conteudo: campo.value})
     ws.send(mensagem)
     campo.value = ""
+}
+
+function newMessage(message){
+    let div_message = document.createElement("div");
+    let id_message = document.createElement("h3");
+    let content_message = document.createElement("h6");
+
+    id_message.innerText = message.ident;
+    content_message.innerText = message.conteudo;
+
+    div_message.appendChild(id_message);
+    div_message.appendChild(content_message);
+
+    div_message.classList.add("message");
+
+    return div_message;
 }
 
 ws.onopen = () => {
@@ -19,11 +35,5 @@ ws.onmessage = (event) => {
 
     let mensagemRecebida = JSON.parse(event.data)
     
-    const p = document.createElement("p")
-    p.innerText = mensagemRecebida.ident
-    mensagens.appendChild(p)
-
-    const p2 = document.createElement("p")
-    p2.innerText = mensagemRecebida.conteudo
-    mensagens.appendChild(p2)
+    mensagens.appendChild(newMessage(mensagemRecebida));
 };
